28b6c2e6fd12de03de8c0d6aecc57991c7c3bf24
Liberty Takehara <libertytakehara@Libertys-MacBook-Air.local>
2025-11-24 21:33:14 +0900

    update

--- docs/web-components/navbar.js
+++ docs/web-components/navbar.js
@@ -5,6 +5,7 @@ class WCNavBar extends HTMLElement {
     template.innerHTML = `
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <base href="https://takehara-biz.github.io/drill-js/" />
     <nav class="navbar navbar-expand-lg bg-body-secondary">
       <div class="container-fluid">
